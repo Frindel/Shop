60ff722fffdcc256c9b50fcee8d18ab70ce4857c
Frindel <matv02@mail.ru>
2024-06-13 12:39:23 +0300

    Добавлено заполнение БД фиктивными данными

--- App.Persistence/AppContext.cs
+++ App.Persistence/AppContext.cs
@@ -12,7 +12,54 @@ namespace App.Persistence
 
         public AppContext(DbContextOptions<AppContext> options) : base(options)
         {
+            //Database.EnsureCreated();
+            InitDbInMemory();
+        }
+
+        static bool isInit = false;
+        public void InitDbInMemory()
+        {
+            if (!Database.IsInMemory() || isInit)
+                return;
+
             Database.EnsureCreated();
+
+            var products = new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Продукт 1",
+                    Price = 100
+                }, new Product()
+                {
+                    Name = "Продукт 2",
+                    Price = 200
+                }, new Product()
+                {
+                    Name = "Продукт 3",
+                    Price = 300
+                }
+            };
+
+            Products.AddRange();
+
+            var admin = new User()
+            {
+                RefreshToken = "xdb04oV0gHEgmGMvWXs7I4k4dMKZTwlZjNJoyUH7af4FA+dQsomacLIQcDGpJyuhNm6XqewoChBgpI2R2e9v0Q==",
+                IsAdmin = true
+            };
+
+            // получение id для продуктов и пользователя
+            SaveChanges();
+
+            Orders.AddRange(new Order()
+            {
+                Products = products,
+            });
+
+            SaveChanges();
+
+            isInit = true;
         }
     }
 }
